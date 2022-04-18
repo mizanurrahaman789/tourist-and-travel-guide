46769d7c9ffecf73cc5414565ba410841d516184
Your Namemizanurrahaman789 <you@example.com>
2022-04-18 11:42:22 -0600

    fotter commit

--- src/Pages/ServiceDetails/ServiceDetails.js
+++ src/Pages/ServiceDetails/ServiceDetails.js
@@ -5,10 +5,10 @@ const ServiceDetails = () => {
       const { serviceId } = useParams
       return (
             <div>
-                  <h2 className='text-center'>Welcome Our Checkout Proceed {serviceId}</h2>
+                  <h2 className='text-center mt-20'>Welcome Our Checkout Proceed {serviceId}</h2>
                   <div className='text-center'>
                         <Link to='/checkout'>
-                              <button className='btn btn-primary'>Proceed Checkout</button>
+                              <button className='btn btn-primary mt-5'>Proceed Checkout</button>
                         </Link>
                   </div>
             </div>
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -4,8 +4,8 @@ import './Footer.css'
 const Footer = () => {
       return (
             <div>
-                  <footer className='footer-containar'>
-                        <p>Copy&copy;Right 2022 Abilable</p>
+                  <footer className='footer-containar '>
+                        <p style={{ color: 'white' }} className=''>Copy&copy;Right 2022 Abilable</p>
                   </footer>
             </div>
       );
